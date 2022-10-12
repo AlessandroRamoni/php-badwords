ffd7f1b116b3dfd1d364f38d442d43fdb5c4ae5e
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-10-12 16:26:55 +0200

    create folder

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First PHP - badwords</title>
+</head>
+
+<body>
+<?php
+    $testo = "holaaaaaaaa"
+    ?>
+
+    <h1>
+      <?php echo $testo ?>
+      hermosa
+    </h1>
+
+    <?php
+    $testo = "Ne parlavamo tanto tanti anni fa
+    Di quanto è paranoica questa città
+    Della sua gente, delle sue manie
+    Due discoteche, centosei farmacie
+    E ci troviamo ancora al punto che
+    Si gira in macchina il mattino alle tre
+    Alla ricerca di qualcosa che poi
+    Cos'è non lo sappiamo nemmeno noi
+    Con un deca non si può andar via
+    Non ci basta neanche in pizzeria
+    Fermati un attimo
+    All'automatico
+    Almeno a piedi non ci lascerà
+    In questa città"
+    ?>
+</body>
+
+</html>
